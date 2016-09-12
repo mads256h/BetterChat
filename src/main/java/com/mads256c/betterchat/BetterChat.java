de0e887bb8ee684682d4c28bb41212738d44b1bc
Mads Mogensen <lort256c@gmail.com>
2016-09-12 17:24:05 +0200

    Delete this

--- src/main/java/com/mads256c/betterchat/BetterChat.java
+++ src/main/java/com/mads256c/betterchat/BetterChat.java
@@ -18,7 +18,7 @@ public class BetterChat
     public static final String MODID = "BetterChat";
     public static final String VERSION = "1.1";
 
-    static Logger bLog;
+    public static Logger bLog;
 
     //region Proxy
     @SidedProxy(clientSide="com.mads256c.betterchat.ClientProxy", serverSide="com.mads256c.betterchat.ServerProxy")
