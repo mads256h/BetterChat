9fa0e58373418a92013af7eeaaee2d42370ce5e2
Mads Mogensen <lort256c@gmail.com>
2016-09-12 20:06:42 +0200

    I fucked up with the gitignore

---
+++ src/main/java/com/mads256c/betterchat/Refrence.java
@@ -0,0 +1,15 @@
+package com.mads256c.betterchat;
+
+/**
+ * Created by Mads on 12-09-2016.
+ */
+public class Refrence {
+
+    public static final  String MODID     = "BetterChat";
+    public static final  String NAME      = "BetterChat";
+    private static final String MAJOR     = "@MAJOR@";
+    private static final String MINOR     = "@MINOR@";
+    private static final String BUILD     = "@BUILD_NUMBER@";
+    private static final String MCVERSION = "@MC_VERSION@";
+    public static final  String VERSION   = MCVERSION + "-" + MAJOR + "." + MINOR + "." + BUILD;
+}
